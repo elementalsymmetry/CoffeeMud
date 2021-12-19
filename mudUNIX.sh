ce6e1f1bdb76beac2c5aca2d569ed525b3e6b174
elementalsymmetry <david.walker@elementalsymmetry.com>
2021-12-19 01:01:09 +0000

    updated for local

--- mudUNIX.sh
+++ mudUNIX.sh
@@ -2,4 +2,4 @@
 #Before using this on a UNIX machine, you must 'chmod 755 mudUNIX.sh' to make this file executable by the UNIX machine
 #FYI - the nohup command will make a nohup.out file, usually in the CofferMud (directory where you start this from) directory - it will log the server messages...
 
-nohup java -classpath ".:./lib/js.jar:./lib/jzlib.jar" -Djava.awt.headless=true -Xms65535000 -Xmx115535000 com.planet_ink.coffee_mud.application.MUD "Your Muds Name Here" &
+nohup /usr/lib/jvm/java-1.16.0-openjdk-amd64/bin/java -classpath ".:./lib/js.jar:./lib/jzlib.jar:/usr/mud/CoffeeMud/lib/mysql-connector-java-8.0.27.jar" -Xms129m -Xmx256m com.planet_ink.coffee_mud.application.MUD "ElementalMud" &
